3a4b4828b25567d94da900ccfb827af233a58361
Gourav Sharma <gouravsharma2806@gmail.com>
2025-08-06 13:35:19 +0530

    main sa modified

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,25 @@
+#declare a region
 variable "region" {
   default = "us-east-1"
 }
 
+#declare a bucket name
 variable "bucket_name_prefix" {
-  default = "fullautomatedbucketterraformone"
+  default = "fullautomatedbucketterraform"
 }
 
+
+#declare a glue job name
 variable "glue_job_name" {
   default = "glue-etl-job"
 }
 
+#declare a crawler name
 variable "glue_crawler_name" {
   default = "my-etl-crawler"
 }
 
+#declare a script path
 variable "script_s3_path" {
-  default = "s3://fullautomatedbucketterraformone/scripts/weather-etl.py"
+  default = "s3://fullautomatedbucketterraform/scripts/weather-etl.py"
 }
-

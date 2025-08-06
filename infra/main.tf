d9c5985030c93dbb12acabd91b9831d0481ea27c
Gourav Sharma <gouravsharma2806@gmail.com>
2025-08-06 13:29:30 +0530

    changed done

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ resource "aws_glue_catalog_database" "etl_db" {
 }
 
 locals {
-  glue_role_arn = "arn:aws:iam::736443288673:role/LabRole"
+  glue_role_arn = "arn:aws:iam::386482925925:role/LabRole"
 }
 
 resource "aws_glue_job" "etl_job" {
